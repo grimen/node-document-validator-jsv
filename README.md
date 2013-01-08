a26603033e89b2771225ed112af865c9cb436d4d
Jonas Grimfelt <grimen@gmail.com>
2013-01-08 20:56:13 +0100

    Init.

---
+++ README.md
@@ -0,0 +1,62 @@
+# NODE-DOCUMENT-VALIDATOR-JSV [![Build Status](https://secure.travis-ci.org/grimen/node-document-validator-jsv.png)](http://travis-ci.org/grimen/node-document-validator-jsv)
+
+**Validator** adapter [jsv](https://github.com/garycourt/JSV) for [node-document](https://github.com/grimen/node-document) ODM for Node.js.
+
+
+## Installation
+
+```shell
+  $ npm install node-document-validator-jsv
+```
+
+
+## Usage
+
+**Basic:**
+
+```javascript
+  var Validator = require('node-document-validator-jsv');
+
+  var validator = new Validator();
+
+  var data = {
+    foo: "foo",
+    bar: 123,
+    baz: undefined
+  };
+
+  var schema = {
+    foo: {
+      type: "string",
+      minLength: 4
+    },
+    bar: {
+      type: "number"
+    },
+    baz: {
+      required: true
+    }
+  };
+
+  validator.validate(data, schema, function(err, errors, valid) {
+    console.log("Valid: %s  \nErrors:\n", valid, errors);
+  });
+```
+
+For details; see [node-document](https://github.com/grimen/node-document).
+
+
+## Test
+
+**Local tests:**
+
+```shell
+  $ make test
+```
+
+
+## License
+
+Released under the MIT license.
+
+Copyright (c) [Jonas Grimfelt](http://github.com/grimen)
---
+++ test/index.js
@@ -0,0 +1,142 @@
+var helper = require('./helper'),
+    assert = helper.assert,
+    debug = helper.debug;
+
+var Validator = require('..'),
+    validator = new Validator(),
+    schema;
+
+// -----------------------
+//  Test
+// --------------------
+
+module.exports = {
+
+  'JSV': {
+    'new': {
+      '()': function() {
+        assert.instanceOf ( validator, require('..') );
+
+        Validator.reset();
+
+        var validator2 = new Validator();
+
+        assert.equal ( validator2.url, null );
+        assert.typeOf ( validator2.options, 'object' );
+        assert.deepEqual ( validator2.options.custom, undefined );
+      },
+
+      '(options)': function() {
+        Validator.reset();
+
+        var validator2 = new Validator({custom: {foo: 'bar'}});
+
+        assert.equal ( validator2.url, null );
+        assert.typeOf ( validator2.options, 'object' );
+        assert.deepEqual ( validator2.options.custom, {foo: 'bar'} );
+      }
+    },
+
+    '.klass': function() {
+      assert.property ( validator, 'klass' );
+      assert.equal ( validator.klass, Validator );
+    },
+
+    '.defaults': function() {
+      assert.property ( Validator, 'defaults' );
+
+      assert.equal ( Validator.defaults.url, null );
+      assert.typeOf ( Validator.defaults.options, 'object' );
+    },
+
+    '.options': function() {
+      assert.property ( Validator, 'options' );
+      assert.typeOf ( Validator.options, 'object' );
+      assert.deepEqual ( Validator.options, {} );
+    },
+
+    '.reset()': function() {
+      assert.property ( Validator, 'reset' );
+      assert.typeOf ( Validator.reset, 'function' );
+
+      Validator.options = {foo: "bar"};
+      assert.deepEqual ( Validator.options, {foo: "bar"} );
+
+      Validator.reset();
+
+      assert.equal ( Validator.url, null );
+    }
+  },
+
+  'JSV.prototype': {
+    '#options': function() {
+      assert.property ( validator, 'options' );
+      assert.typeOf ( validator.options, 'object' );
+    },
+
+    '#engine': function() {
+      assert.property ( validator, 'engine' );
+      assert.deepEqual ( validator.engine, require('JSV').JSV );
+    },
+
+    '#validate': {
+      before: function() {
+        schema = {
+          title: {
+            required: true,
+            type: 'string',
+            minLength: 7
+          }
+        };
+      },
+
+      '': function() {
+        assert.property ( validator, 'validate' );
+        assert.typeOf ( validator.validate, 'function' );
+      },
+
+      '(attributes) - when valid data': function(done) {
+        var data = {title: "A title"};
+
+        validator.validate(data, schema, function(err, errors, valid) {
+          assert.typeOf ( errors, 'null' );
+          assert.equal ( valid, true );
+          done();
+        });
+      },
+
+      '(attributes) - when invalid data': function(done) {
+        var data = {title: "A"};
+
+        validator.validate(data, schema, function(err, errors, valid) {
+          assert.typeOf ( errors, 'array' );
+          assert.equal ( errors.length, 1 );
+          assert.equal ( valid, false );
+          done();
+        });
+      },
+
+      '(attributes, options) - when valid data': function(done) {
+        var data = {title: "A title"};
+
+        validator.validate(data, schema, {}, function(err, errors, valid) {
+          assert.typeOf ( errors, 'null' );
+          assert.equal ( valid, true );
+          done();
+        });
+      },
+
+      '(attributes, options) - when invalid data': function(done) {
+        var data = {title: "A"};
+
+        validator.validate(data, schema, {}, function(err, errors, valid) {
+          assert.typeOf ( errors, 'array' );
+          assert.equal ( errors.length, 1 );
+          assert.equal ( valid, false );
+          done();
+        });
+      }
+    }
+  }
+
+};
